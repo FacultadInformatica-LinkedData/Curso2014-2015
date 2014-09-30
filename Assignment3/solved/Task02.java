59c324c112982396a15edd43c56a8a5285e31cf4
alejandro2003 <alejandro2003@ukr.net>
2014-10-01 01:27:08 +0200

    tarea2

--- Assignment3/solved/Task02.java
+++ Assignment3/solved/Task02.java
@@ -4,6 +4,7 @@ import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
 import com.hp.hpl.jena.sparql.vocabulary.FOAF;
 import com.hp.hpl.jena.vocabulary.VCARD;
 
@@ -29,21 +30,19 @@ public class Task02
 		String johnURI = ns+"JohnSmith";
 		
 		// ** TASK 2.1: Create the resource John Smith
-		Resource johnSmith = model.createResource(johnURI);
+		Resource johnSmith = ResourceFactory.createResource("John Smith");
 
 		// Add to johnSmith the datatype property full name (from the VCARD vocabulary)
-		johnSmith.addLiteral(VCARD.FN, fullName);
+		johnSmith.addProperty(VCARD.FN, fullName);
 		
 		// ** TASK 2.2: Create a new resource for Jane Smith, specifying her full name and email **
-		Resource janeSmith = model.createResource(ns+"JaneSmith");
-		janeSmith.addLiteral(VCARD.FN, "Jane Smith");
-		janeSmith.addLiteral(VCARD.EMAIL, "jSmith@somewhere.com");
+		Resource janeSmith = ResourceFactory.createResource("Jane Smith");
+		janeSmith.addProperty(VCARD.FN, "Jane Smith");
+		janeSmith.addProperty(VCARD.EMAIL, "janeSmith2@mail.com");		
 		
 		// ** TASK 2.3: Add Jane as a person who John knows through an object property from the FOAF vocabulary**
-//		Property knows = model.createProperty(foafNS+"knows");		
-//		johnSmith.addProperty(knows, janeSmith);
-		
 		johnSmith.addProperty(FOAF.knows, janeSmith);
+		
 		
 		model.write(System.out, "RDF/XML-ABBREV");
 	}
