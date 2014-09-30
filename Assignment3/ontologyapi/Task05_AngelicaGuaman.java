5c115cbef33e320adf95b48d4eeab3732625e048
Angelica Guaman <angelica.guaman.albarracin@gmail.com>
2014-09-30 15:33:34 +0200

    Entrega Java

---
+++ Assignment3/ontologyapi/Task05_AngelicaGuaman.java
@@ -0,0 +1,37 @@
+package ontologyapi;
+
+import java.io.InputStream;
+
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.util.FileManager;
+
+/**
+ * Task 05: Reading and writing ontologies (RDFs)
+ * @author elozano
+ *
+ */
+public class Task05
+{
+	public static void main(String args[])
+	{
+		String filename = "example4.rdf";
+		
+		// Create an empty model
+		OntModel model = ModelFactory.createOntologyModel(OntModelSpec.RDFS_MEM);
+		
+		// ** TASK 5.1: Read the ontology from the given file **
+		// Use the FileManager to find the input file
+		InputStream in = FileManager.get().open(filename);
+	
+		// Read the RDF/XML file
+		model.read(in, null);
+		
+		// ** TASK 5.2: Write the ontology **
+		
+		
+		//model.write(System.out, "RDF/XML-ABBRV");
+		model.write(System.out, "TURTLE");
+	}
+}
